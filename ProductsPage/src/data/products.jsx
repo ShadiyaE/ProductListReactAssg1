3a15fbe7308cde655f5ceb728b290f9ec5b3d2d7
ShadiyaE <shadiya.kareem123@gmail.com>
2025-05-27 14:24:21 +0300

    first commit

---
+++ ProductsPage/src/data/products.jsx
@@ -0,0 +1,52 @@
+// src/data/products.js
+export const products = [
+  {
+    id: 1,
+    name: 'Product Name 1',
+    price: 1000,
+    category: 'Category A',
+    rating: 4.5,
+    image: 'https://m.media-amazon.com/images/I/61eX8GCN1EL._AC_UL320_.jpg',
+  },
+  {
+    id: 2,
+    name: 'Product Name 2',
+    price: 2005,
+    category: 'Category B',
+    rating: 4.0,
+    image: 'https://m.media-amazon.com/images/I/71bSm2ItCqL._AC_UL320_.jpg',
+  },
+  {
+    id: 3,
+    name: 'Product Name 3',
+    price: 1550,
+    category: 'Category A',
+    rating: 3.5,
+    image: 'https://m.media-amazon.com/images/I/61gkyiHVb2L._AC_UL320_.jpg',
+  },
+  {
+    id: 4,
+    name: 'Product Name 4',
+    price: 3000,
+    category: 'Category C',
+    rating: 4.8,
+    image: 'https://m.media-amazon.com/images/I/61YDKY7uuvL._AC_UL320_.jpg',
+  },
+  {
+    id: 5,
+    name: 'Product Name 5',
+    price: 3070,
+    category: 'Category C',
+    rating: 4.8,
+    image: 'https://m.media-amazon.com/images/I/71lo++IOYjL._AC_UL320_.jpg',
+  },
+  {
+    id: 6,
+    name: 'Product Name 6',
+    price: 3900,
+    category: 'Category C',
+    rating: 4.8,
+    image: 'https://m.media-amazon.com/images/I/610U8JWpp9L._AC_UL320_.jpg',
+  },
+  // Add more products as needed
+];
